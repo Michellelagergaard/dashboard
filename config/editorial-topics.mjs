1458d1c6d234e6e604fa37b80e5e550d89eddf86
Michellelagergaard <mil@dp.dk>
2026-09-24 03:56:48 -0700

    Improve newsletter categories

--- config/editorial-topics.mjs
+++ config/editorial-topics.mjs
@@ -1,24 +1,28 @@
 export const editorialTopicNames = [
-  "Praksis og ydernummer",
-  "Løn og arbejdsliv",
-  "Autorisation og regler",
-  "Faglighed og forskning",
+  "Politik og interessevaretagelse",
+  "Løn, overenskomst og ansættelse",
+  "Arbejdsmiljø og trivsel",
+  "Autorisation, tilsyn og regler",
+  "Faglighed og praksis",
+  "Karriere og job",
   "Kurser og arrangementer",
-  "Medlemsfordele og foreningsliv",
-  "Job og karriere",
-  "Politik og presse",
+  "Medlemsfordele",
+  "Foreningsliv og demokrati",
+  "Praksis og ydernummer",
   "Andet",
 ];
 
 const rules = [
-  ["Praksis og ydernummer", /ydernummer|selvstændig|selvstaendig|praksis|honorar|budgetlægning|budgetlaegning|regionernes.udbud|\bpok\b/],
-  ["Løn og arbejdsliv", /overenskomst|\bok\d{2}\b|\bløn\b|\bloen\b|lønstign|loenstign|rettighed|ferie|arbejdsliv|arbejdsmiljø|arbejdsmiljo|barsel|pension/],
-  ["Autorisation og regler", /autorisation|tilsyn|journalføring|journalforing|klage|lovgivning|lovforslag|persondata|gdpr/],
-  ["Job og karriere", /psykologjob|ledige.still|karriere|\bjob\b|dimittend|vej ind i psykologfaget/],
-  ["Faglighed og forskning", /faglig|forsk|psykologfag|behandling|psykiatri|\bppr\b|\bicd|videnscenter|supervision/],
-  ["Kurser og arrangementer", /arrangement|webinar|fyraftensmøde|fyraftensmode|kursus|kurser|konference/],
-  ["Medlemsfordele og foreningsliv", /medlemsfordel|tivoli|rabat|forbrugsforening|generalforsamling|\bgf\d{2}\b/],
-  ["Politik og presse", /politik|folketing|\bvalg\b|høring|horing|presse|reaktion|generalforsamling|\bgf\d{2}\b/],
+  ["Medlemsfordele", /medlemsfordel|tivoli|rabat|forbrugsforening|politiken|aarstiderne|årstiderne|filmklub|forsikringstilbud/],
+  ["Foreningsliv og demokrati", /generalforsamling|\bgf\d{2}\b|repræsentantskab|repraesentantskab|kredsvalg|sektionsvalg|bestyrelsesvalg|arbejdsprogram|frivillig.enhed|kreds|sektion|selskab|netværk|netvaerk/],
+  ["Praksis og ydernummer", /ydernummer|selvstændig|selvstaendig|praksisdrift|praksisoverenskomst|honorar|timepris|budgetlægning|budgetlaegning|\bpok\b/],
+  ["Løn, overenskomst og ansættelse", /overenskomst|\bok\d{2}\b|\bløn\b|\bloen\b|lønstign|loenstign|lokal.løn|lokal.loen|ansættelse|ansaettelse|ansættelsesvilkår|ansaettelsesvilkaar|arbejdstid|ferie|barsel|pension|frit.valg/],
+  ["Arbejdsmiljø og trivsel", /arbejdsmiljø|arbejdsmiljo|\bamr\b|\bapv\b|trivsel|stress|sygefravær|sygefravaer|psykisk.arbejdsmiljø|psykisk.arbejdsmiljo|moralsk.stress/],
+  ["Autorisation, tilsyn og regler", /autorisation|autorisationsordning|tilsyn|journalføring|journalforing|journaloplysning|klage|patientklage|lovgivning|persondata|gdpr|tavshedspligt|samtykke|titelbeskyttelse|ppu/],
+  ["Karriere og job", /psykologjob|ledige.still|karriere|\bjob\b|jobsøgning|jobsoegning|dimittend|cv\b|ansøgning|ansoegning|vej ind i psykologfaget|lederrolle/],
+  ["Kurser og arrangementer", /arrangement|webinar|fyraftensmøde|fyraftensmode|morgenmøde|morgenmoede|kursus|kurser|konference|temamøde|temamoede|tilmeld/],
+  ["Politik og interessevaretagelse", /politik|politisk|folketing|regering|minister|høring|horing|udspil|reform|interessevaretagelse|presse|skarp.kritik|udbudssag|regionernes.udbud/],
+  ["Faglighed og praksis", /faglig|forsk|psykologfag|behandling|psykiatri|\bppr\b|\bicd|videnscenter|supervision|faglig.norm|retningslinje|evidens/],
 ];
 
 export function editorialCategory(item) {
